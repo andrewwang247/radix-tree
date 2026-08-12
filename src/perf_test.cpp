3301ba122e0607a4c2fa670571c59285949bb4a0
Andrew Wang <siweiw9@gmail.com>
2026-08-11 18:14:57 -0700

    format messaging along columns

--- src/perf_test.cpp
+++ src/perf_test.cpp
@@ -39,6 +39,7 @@ namespace views = std::views;
 int main() {
   ios_base::sync_with_stdio(false);
   static constexpr size_t SAMPLE_SIZE = 2500;
+  static constexpr auto ANNOUNCE_TEMPLATE = "{:<18}";
 
   default_random_engine prng{random_device{}()};  // NOLINT(whitespace/braces)
   const auto words = perf_test::read_words(prng);
@@ -50,31 +51,31 @@ int main() {
   set_perf set_benchmark;
   trie_perf trie_benchmark;
 
-  cout << "Insert words: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Insert words:");
   perf_test::show_comparison(set_benchmark.insert(words),
                              trie_benchmark.insert(words));
 
-  cout << "Count prefix: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Count prefix:");
   perf_test::show_comparison(set_benchmark.count(solutions),
                              trie_benchmark.count(solutions));
 
-  cout << "Find prefix: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Find prefix:");
   perf_test::show_comparison(set_benchmark.find(solutions),
                              trie_benchmark.find(solutions));
 
-  cout << "Contains words: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Contains words:");
   perf_test::show_comparison(set_benchmark.contains(sublist),
                              trie_benchmark.contains(sublist));
 
-  cout << "Forward iterate: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Forward iterate:");
   perf_test::show_comparison(set_benchmark.forward_iterate(),
                              trie_benchmark.forward_iterate());
 
-  cout << "Reverse iterate: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Reverse iterate:");
   perf_test::show_comparison(set_benchmark.reverse_iterate(),
                              trie_benchmark.reverse_iterate());
 
-  cout << "Erase prefix: ";
+  cout << format(ANNOUNCE_TEMPLATE, "Erase prefix:");
   perf_test::show_comparison(set_benchmark.erase(solutions),
                              trie_benchmark.erase(solutions));
 
@@ -256,12 +257,14 @@ timeunit_t trie_perf::erase(const vector<perf_test::solution_t>& solutions) {
 }
 
 void perf_test::show_comparison(timeunit_t set_time, timeunit_t trie_time) {
+  static constexpr auto COMPARE_TEMPLATE =
+      "{:>4} was {:4.1f} x faster than {:>4}\n";
   const auto diff_ratio =
       static_cast<double>(std::max(set_time, trie_time).count()) /
       static_cast<double>(std::min(set_time, trie_time).count());
   if (set_time < trie_time) {
-    cout << format("set was {:.1f} x faster than trie\n", diff_ratio);
+    cout << format(COMPARE_TEMPLATE, "set", diff_ratio, "trie");
   } else {
-    cout << format("trie was {:.1f} x faster than set\n", diff_ratio);
+    cout << format(COMPARE_TEMPLATE, "trie", diff_ratio, "set");
   }
 }
